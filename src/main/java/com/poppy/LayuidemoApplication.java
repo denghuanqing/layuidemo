d2aa228d4dea4175b2f162bd84aaab4d03b906e9
Poppy <denghuan1996@163.com>
2018-06-04 23:09:29 +0800

    提交 git ignore 初始化提交

---
+++ src/main/java/com/poppy/LayuidemoApplication.java
@@ -0,0 +1,12 @@
+package com.poppy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayuidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayuidemoApplication.class, args);
+	}
+}
